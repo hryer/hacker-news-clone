14ca39ff7914975e1ba918a1bf218af7afc6cbc6
Hryer <harryermawan@gmail.com>
2019-02-04 23:55:24 +0700

    Queries loading link

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../styles/App.css';
+import styled from 'styled-components';
+import LinkList from './LinkList';
 
+const Header = styled.div`
+  background-color: black;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      < LinkList />
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/index.css'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
