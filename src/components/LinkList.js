4c7d96b3fcac1588a3dfea9031e6eb0039e94eee
Hryer <harryermawan@gmail.com>
2019-02-06 17:21:40 +0700

    Queries

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,28 +1,45 @@
 import React, {Component} from 'react';
 import Link from './Link';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
+const FEED_QUERY = gql`
+    {
+        feed {
+            links {
+                id
+                createdAt
+                url
+                description
+            }
+        }
+    }
+`
 class LinkList extends Component {
     render() {
-        const linksToRender =  [
-            {
-                id: '1',
-                description: 'Prisma replaces traditional ORMs and makes it easy to build GraphQL servers 😎',
-                url: 'https://www.prisma.io',
-            },
-            {
-                id: '2',
-                description: 'The best GraphQL client',
-                url: 'https://www.apollographql.com/docs/react/',
-            },
-        ];
-
         return (
-            <div>
-                {linksToRender.map((link)=> 
-                    <Link key={link.id} link={link} />
-                )}
-            </div>
-        );
+            <Query query={FEED_QUERY}>
+                {({loading,error,data}) => {
+                    if(loading){
+                        return (<div>Fetching...</div>)
+                    }
+
+                    if(error){
+                        return (<div>Error... We'll fix it be patience...</div>)
+                    }
+                    const linksToRender = data.feed.links;
+
+                    return (  
+                        linksToRender.map( function(link){ 
+                            return(
+                                <Link key={link.id} link={link} description={link.description} />
+
+                            )
+                        }) 
+                    )
+                }}
+            </Query>
+        )
     }
 
 }
